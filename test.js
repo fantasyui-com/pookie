cc71287bcd56e967cd16693b8f353e89e09d6a89
fantasyui-com <captain@fantasyui.com>
2018-06-23 10:17:22 -0400

    Upgrade DSL to spec

--- test.js
+++ test.js
@@ -1,26 +1,28 @@
 const uuidv4 = require('uuid/v4');
-const {Tree, Root, Branch} = require('./index.js');
-const data = function(pipe){
+const {Tree, Root, Branch} = require('./core.js');
 
-  const record = function(){
+// const data = function(pipe){
+//
+//   const record = function(){
+//
+//     const object = {};
+//
+//     object.uuid = uuidv4();
+//     object.version = (new Date).getTime();
+//     object.class = (new Date).getTime();
+//
+//     return object;
+//
+//   }
+//
+//   const a = setInterval(function(){pipe(record())},3*1000)
+//   const b = setInterval(function(){pipe(record())},5*1000)
+//   const c = setInterval(function(){pipe(record())},8*1000)
+//
+//   setTimeout(function(){[a,b,c].map(i=>clearInterval(i))},60*1000)
+//
+// }
 
-    const object = {};
-
-    object.uuid = uuidv4();
-    object.version = (new Date).getTime();
-    object.class = (new Date).getTime();
-
-    return object;
-
-  }
-
-  const a = setInterval(function(){pipe(record())},3*1000)
-  const b = setInterval(function(){pipe(record())},5*1000)
-  const c = setInterval(function(){pipe(record())},8*1000)
-
-  setTimeout(function(){[a,b,c].map(i=>clearInterval(i))},60*1000)
-
-}
 const root = new Root();
 
 let map = Tree.decodeMap(`
@@ -32,31 +34,36 @@ let map = Tree.decodeMap(`
   make Applications/Todo/Today today
   make Applications/Todo/Today/Priority priority,hot,important
 
-  make Applications/Debugger debug
+  make Applications/Debugger *,debug
   make Applications/Debugger/Success success
   make Applications/Debugger/Danger danger
   make Applications/Debugger/Warning warning
   make Applications/Debugger/Info info
 
 `);
+
 Tree.importMap(root, map);
 
 
 // console.log( util.inspect(root, { showHidden: true, depth: null }) );
 // console.log( root.locate('Root/Users') )
-console.log( root.locate('Root') )
+// console.log( root.locate('Root') )
 
+const priorityBranch = root.locate('Applications/Todo/Today/Priority');
+priorityBranch.on('content', function(object){
+  console.log('Got Object', object)
+})
 
-let binding = Tree.decodeMap(`
+const object1 = {
+  uuid: uuidv4(),
+  version: (new Date).getTime(),
+  tags: "todo,today,priority",
+}
+const object2 = {
+  uuid: uuidv4(),
+  version: (new Date).getTime(),
+  tags: "today,priority",
+}
 
-  # Main Objects
-  pipe server-broadcast Root
-
-  # Messages to display
-  apply message Root/Users/Admin/Messages
-  apply message Root/Users/System/Messages
-
-`);
-
-
-Tree.importData(root, data);
+root.pipe(object1)
+root.pipe(object2)
